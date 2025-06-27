b814bf59379b6328489421f96e6574b35b18aa78
vitor gamarano <146540200+Vitor-S-G-C@users.noreply.github.com>
2025-06-27 16:54:41 -0300

    Update Salario.java

--- src/Entits/Salario.java
+++ src/Entits/Salario.java
@@ -2,7 +2,7 @@ package entits;
 
 public class Salario {
 
-    //caso queira escolher o valor do salario basta deixar a linha de baico assim: public double salarioAnterior;
+    //caso queira escolher o valor do salario basta deixar a linha de baixo assim: public double salarioAnterior;
     public double salarioAnterior = 6000.00;
     public int aumento;
 
